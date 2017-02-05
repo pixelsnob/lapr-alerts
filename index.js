3bf4f3430b8edd0b1d120a6084118e18b3059da3
Luis A. Echeverria <snob@pixelsnob.com>
2017-02-05 13:22:33 -0800

    Cleanup, optimization

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 
 const casper = require('casper').create(),
       fs     = require('fs'),
+      system = require('system'),
       config = require('./config.json');
 
 const url = 'https://accounts.google.com/ServiceLogin?hl=en&service=alerts' +
             '&continue=http://www.google.com/alerts/manage'; 
 
 const selectors = {
-  login_form:   'form#gaia_loginform',
-  input:        '.query_div input[type=text]',
-  submit:       '#create_alert',
-  results:      '.page-content .my_alerts'
+  login_form:     'form#gaia_loginform',
+  passwd:         '#Passwd',
+  signin:         '#signIn',
+  next:           '#next',
+  input:          '.query_div input[type=text]',
+  submit:         '#create_alert',
+  results:        '.page-content .my_alerts',
+  captcha:        '#captcha',
+  captcha_submit: 'input[name="submit"]'
 };
 
 var stream  = fs.open('./data/alerts.csv', 'r'),
@@ -25,33 +31,39 @@ while (line = stream.readLine()) {
 
 casper.start(url, function() {
   this.waitForSelector(selectors.login_form);
-}).then(function() {
-  // Login
+}).then(function login() {
   this.echo('Logging in to ' + this.getTitle());
   this.fill(selectors.login_form, { Email: config.email });
-  this.click('#next'); 
-  //this.wait(500);
-  this.waitForSelector("#Passwd");
-}).then(function() {
+  this.click(selectors.next); 
+  this.waitForSelector(selectors.passwd);
+}).then(function password() {
   // Password
   this.fill(selectors.login_form, { Passwd: config.password });
-  this.click("#signIn"); 
-  this.waitForSelector(selectors.input);
-}).then(function() {
+  this.click(selectors.signin); 
+  this.waitForSelector(selectors.input, null, function timeout() {
+    // See if there's a captcha
+    this.capture('captures/captcha.png');
+    // Pause script until value is provided
+    system.stdout.writeLine('Provide captcha value to continue: ');
+    this.sendKeys(selectors.captcha, system.stdin.readLine());
+    this.click(selectors.captcha_submit);
+    this.waitForSelector(selectors.input);
+  });
+}).then(function addAlerts() {
   this.echo('Signed in as ' + config.email);
-}).then(function() {
   // Add alerts 
-  var i = 0;
+  var i = 1;
   this.echo('Attempting to add ' + alerts.length + ' alerts');
   this.each(alerts, function(self, line) {
+    // Remove leading space
     line = line.replace(/^\s/, '');
     // Some time between requests to keep us from being banned
-    this.wait(6000, function() {
-      this.echo(line);
+    this.wait(500, function() {
       this.sendKeys(selectors.input, line, { keepFocus: true });
       this.waitForSelector(selectors.submit, function() {
         this.click(selectors.submit);
-        this.echo(i + '. ' + line);
+        this.echo(i + '. [Added] ' + line);
+        i++;
       });
     });
   });
